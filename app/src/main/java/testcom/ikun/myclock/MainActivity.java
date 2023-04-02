d4285b6a2c4fcd485cc336b97a3141c40ae7fc04
lswlc33 <lswlc33@qq.com>
2023-04-02 17:08:27 +0800

    update

--- app/src/main/java/testcom/ikun/myclock/MainActivity.java
+++ app/src/main/java/testcom/ikun/myclock/MainActivity.java
@@ -2,6 +2,7 @@ package testcom.ikun.myclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,6 +21,16 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     public static boolean allowcolorchange = true;
     public static boolean allowtextchange = true;
+
+    public static int textsize = 50;
+
+    public static int getTextsize() {
+        return textsize;
+    }
+
+    public static void setTextsize(int textsize) {
+        MainActivity.textsize = textsize;
+    }
 
     public static boolean isAllowcolorchange() {
         return allowcolorchange;
@@ -36,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.allowtextchange = allowtextchange;
     }
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         FrameLayout background = (FrameLayout) findViewById(R.id.background);
         TextView text = (TextView) findViewById(R.id.textView);
         ImageView image = (ImageView) findViewById(R.id.imageView2);
+        text.setTextSize(textsize);
 
         image.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         background.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 int index = random.nextInt(colors.size());
                 int index2 = random.nextInt(emojitext.size());
 
+                text.setTextSize(textsize);
 
                 if (allowcolorchange) {
                     background.setBackgroundColor(Color.parseColor(colors.get(index)));
@@ -98,7 +115,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setTextSize(textsize);
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
--- app/src/main/java/testcom/ikun/myclock/Setting.java
+++ app/src/main/java/testcom/ikun/myclock/Setting.java
@@ -1,14 +1,27 @@
 package testcom.ikun.myclock;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class Setting extends AppCompatActivity {
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +29,31 @@ public class Setting extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Switch S1 = (Switch) findViewById(R.id.switch2);
         Switch S2 = (Switch) findViewById(R.id.switch3);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar2);
+
 
         S1.setChecked(MainActivity.isAllowcolorchange());
         S2.setChecked(MainActivity.isAllowtextchange());
 
+        seekBar.setProgress(MainActivity.getTextsize());
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                MainActivity.setTextsize(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         S1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
